a3765843bc38899bb71d546404f10908aa28a220
Jesus guerra <guerrajesus@live.com>
2025-02-22 23:07:45 -0800

    Week 13 update

---
+++ src/main/java/pet/store/entity/Employee.java
@@ -0,0 +1,36 @@
+package pet.store.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //tells JPA how primary key is managed, in this case by mySQL
+	private Integer employeeId;  //lower camel case 
+	
+	private String employeeFirstName;
+	private String employeeLastName;
+	private String employeePhone;
+	private String employeeJobTitle;
+	
+	
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pet_store_id") //use the snack_case 
+	private PetStore petStore;
+	
+	
+	
+}
